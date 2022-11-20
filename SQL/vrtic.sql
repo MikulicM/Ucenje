6cc0d493366273827ea5378b53eefde039e67ffc
MikulicM <117686335+MikulicM@users.noreply.github.com>
2022-11-20 17:18:40 +0100

    Update vrtic.sql

--- SQL/vrtic.sql
+++ SQL/vrtic.sql
@@ -13,7 +13,8 @@ create table djete(
     sifra int not null primary key auto_increment,
     ime varchar(50),
     prezime varchar(50),
-    oib char(11)
+    oib char(11),
+    skupina int
 );
 
 create table odgajateljica(
@@ -27,8 +28,10 @@ create table strucnasprema(
     sifra int not null primary key auto_increment,
     koja varchar(50),
     gdje varchar(50),
-    kada datetime
+    kada datetime,
+    odgajateljica int
 );
 
 alter table skupina add foreign key (odgajateljica) references odgajateljica(sifra);
-alter table djete add foreign key (djete) references skupina(sifra);
+alter table djete add foreign key (skupina) references skupina(sifra);
+alter table strucnasprema add foreign key (odgajateljica) references odgajateljica(sifra);
