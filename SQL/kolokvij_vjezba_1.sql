d10266aba454678d62db2bd15076e7b9e22ed8df
MikulicM <117686335+MikulicM@users.noreply.github.com>
2022-12-01 15:31:02 +0100

    Dodavanje veza

--- SQL/kolokvij_vjezba_1.sql
+++ SQL/kolokvij_vjezba_1.sql
@@ -76,3 +76,9 @@ create table cura(
     punac int
 );
 
+alter table sestra_svekar add foreign key (sestra) references sestra(sifra);
+alter table sestra_svekar add foreign key (svekar) references svekar(sifra);
+alter table zena add foreign key (sestra) references sestra(sifra);
+alter table muskarac add foreign key (zena) references zena(sifra);
+alter table mladic add foreign key (muskarac) references muskarac(sifra);
+alter table cura add foreign key (punac) references punac(sifra);
